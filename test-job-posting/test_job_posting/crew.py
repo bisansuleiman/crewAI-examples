db38d1ec801bc616851787ebca93d1315d0ac1f8
Bisan Suleiman <bisansuleiman@berkeley.edu>
2024-07-25 12:49:20 -0700

    edited main.py

--- test-job-posting/test_job_posting/crew.py
+++ test-job-posting/test_job_posting/crew.py
@@ -91,11 +91,10 @@ class JobPostingCrew():
 
     @crew
     def crew(self) -> Crew: 
-      """Creates the JobPostingCrew"""
-      return Crew(
+        """Creates the JobPostingCrew"""
+        return Crew(
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
 			process=Process.sequential,
 			verbose=2,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
-		)
+        )
--- test-job-posting/test_job_posting/main.py
+++ test-job-posting/test_job_posting/main.py
@@ -5,10 +5,10 @@ from test_job_posting.crew import JobPostingCrew
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
-        'company_domain':'crewai.com',
-        'company_descriptio': 'crewai.com',
-        'hiring_needs': '',
-        'specific_benefits':'',
+        'company_domain':'baskinrobins.com',
+        'company_description': 'Baskin-Robbins is an American multinational chain of ice cream and cake specialty shops owned by Inspire Brands',
+        'hiring_needs': 'Cake decorator,  at least 1 - 2 years cake decorating experience, stand for long periods of time, cake decorator needs',
+        'specific_benefits':'Weekly Pay, Employee Meals',
     }
     JobPostingCrew().crew().kickoff(inputs=inputs)
 
@@ -18,10 +18,10 @@ def train():
     Train the crew for a given number of iterations.
     """
     inputs = {
-        'company_domain':'crewai.com',
-        'company_descriptio': 'crewai.com',
-        'hiring_needs': '',
-        'specific_benefits':'',
+        'company_domain':'baskinrobins.com',
+        'company_description': 'Baskin-Robbins is an American multinational chain of ice cream and cake specialty shops owned by Inspire Brands',
+        'hiring_needs': 'Cake decorator,  at least 1 - 2 years cake decorating experience, stand for long periods of time, cake decorator needs',
+        'specific_benefits':'Weekly Pay, Employee Meals',
     }
     try:
         JobPostingCrew().crew().train(n_iterations=int(sys.argv[1]), inputs=inputs)
